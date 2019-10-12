b598a0819df1cc6111335a94d764303267d5cba5
Maia Salholz-Hillel <mash2015@gmail.com>
2019-10-12 13:00:12 +0200

    Updated lunch assignment 2019-10-10 to reflect JG's script

--- code/2019-10-10_lunch-assignment_msh.R
+++ code/2019-10-10_lunch-assignment_msh.R
@@ -1,45 +1,68 @@
-# assignment
+# Assignment
 # Use the 2019Q2 SEC data to extract current quarterly revenues of U.S. based firms. Try to replicate the following methodology. We use the SEC Financial Statement Dataset to obtain the most current quarterly revenues of U.S. based SEC registrants. To distill total revenue from reported XBRL data, we take for each filing the maximum value of the three tags
 # "Revenues"
 # "RevenueFromContractWithCustomerExcludingAssessedTax"
 # "RevenueFromContractWithCustomerIncludingAssessedTax"
 # Produce a clean sample and report the sample size, the sample’s mean and the sample’s standard deviation.
 # For extra credit: Prepare a map that shows where in the U.S. the corporate revenues are located.
+# Data dictionary from: https://www.sec.gov/files/aqfs.pdf
 
-# load packages
 library(tidyr)
 library(dplyr)
 library(readr)
 library(tidylog)
 library(stringr)
 
-# read in csvs 
 num_df <- read_csv("data/num.csv")
 pre_df <- read_csv("data/pre.csv")
 sub_df <- read_csv("data/sub.csv")
 tag_df <- read_csv("data/tag.csv")  
 
-# filter for variables of interest
-sub_df %>% 
-  filter(countryba == "US") %>%
-  filter(str_detect(fp, "Q")) %>% 
-
-
-filter(
-  tag == "Revenues" |
-    tag == "RevenueFromContractWithCustomerExcludingAssessedTax" | 
+# Extract current quarterly revenues of U.S. based firms
+revenue <-
+  sub_df %>%
+  filter(
+    countryba == "US",
+    str_detect(fp, "Q")
+  ) %>%
+  select(adsh, cik, name, sic, fp, period, fye) %>%
+  group_by(cik) %>%
+  top_n(1, wt = period) %>%
+  left_join(pre_df) %>%
+  filter(
+    stmt == "IS",
+    tag == "Revenues" |
+    tag == "RevenueFromContractWithCustomerExcludingAssessedTax" |
     tag == "RevenueFromContractWithCustomerIncludingAssessedTax"
-)
+  ) %>%
+  distinct(adsh, tag, version, .keep_all = TRUE) %>%
+  left_join(num_df) %>%
+  filter(
+    ddate == period,
+    qtrs == 1L,
+    is.na(coreg),
+    uom == "USD",
+    !is.na(value)
+  ) %>%
+  mutate(total_revenue = value)  %>%
+  select(adsh, cik, name, sic, fp, fye, ddate, total_revenue) %>%
+  group_by(adsh, cik, name, sic, fp, fye, ddate) %>%
+  top_n(1, wt = total_revenue) %>%
+  ungroup() %>%
+  distinct(cik, name, sic, fp, fye, ddate, .keep_all = TRUE) %>%
+  arrange(cik)
 
+# Explore current quarterly revenues of U.S. based firms
+nrow(revenue)
 
-# create joined dataframe
-# join by columns taken from: https://www.sec.gov/files/aqfs.pdf
-sec_df <-
-  num_df %>% 
-  left_join(pre_df, by = "adsh") %>%
-  # left_join(tag_df, by = "version") %>% 
-  left_join(pre_df, by = "adsh")
+length(unique(revenue$cik)) == nrow(revenue)
+table(revenue$ddate)
 
-# select revenue column
-sec_df %>% 
-  select(value)
+revenue_descriptives <- 
+  revenue %>%
+  select(-ddate, -cik) %>% 
+  ExPanDaR::prepare_descriptive_table()
+
+revenue_descriptives$df
+
+revenue %>% arrange(-total_revenue)
